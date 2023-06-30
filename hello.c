6bb6e7ca4ee5ac330784992fbe23a800859eb4f6
eswarpothabattula <eswar5802@gmail.com>
2023-06-30 23:16:20 +0530

    Linked list program

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	printf("Hai Veera\n");
-	printf("Hello Veera\n");
-}
--- linked_list.c
+++ linked_list.c
@@ -1,257 +1,142 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-struct node {
-   int data;
-   int key;
-   struct node *next;
+struct node
+{
+    int data;
+    struct node *next;
 };
 
-struct node *head = NULL;
-struct node *current = NULL;
-
-//display the list
-void printList() {
-   struct node *ptr = head;
-   printf("\n[ ");
-	
-   //start from the beginning
-   while(ptr != NULL) {
-      printf("(%d,%d) ",ptr->key,ptr->data);
-      ptr = ptr->next;
-   }
-	
-   printf(" ]");
+void add(struct node **head, int data)
+{
+    struct node *new = (struct node*)malloc(sizeof(struct node));
+    new->data = data;
+    new->next = *head;
+    *head = new;
 }
 
-//insert link at the first location
-void insertFirst(int key, int data) {
-   //create a link
-   struct node *link = (struct node*) malloc(sizeof(struct node));
-	
-   link->key = key;
-   link->data = data;
-	
-   //point it to old first node
-   link->next = head;
-	
-   //point first to new first node
-   head = link;
+void print(struct node *head)
+{
+    struct node *temp = head;
+    while(temp != NULL)
+    {
+        printf("%d -> ",temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
 }
 
-//delete first item
-struct node* deleteFirst() {
-
-   //save reference to first link
-   struct node *tempLink = head;
-	
-   //mark next to first link as first 
-   head = head->next;
-	
-   //return the deleted link
-   return tempLink;
+void print_address(struct node *head)
+{
+    struct node *temp = head;
+    while(temp != NULL){
+        printf("%d\t %p\t %p\n",temp->data,&(temp->data),temp->next);
+        temp = temp->next;
+    }
 }
 
-//is list empty
-bool isEmpty() {
-   return head == NULL;
+void reverse(struct node **head)
+{
+    struct node *cur = *head;
+    struct node *prev = NULL,*next = NULL;
+    while(cur !=NULL){
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    *head = prev;
 }
 
-int length() {
-   int length = 0;
-   struct node *current;
-	
-   for(current = head; current != NULL; current = current->next) {
-      length++;
-   }
-	
-   return length;
+void last(struct node **head)
+{
+    struct node *new,*temp;
+    new = (struct node *)malloc(sizeof(struct node));
+    int data;
+    if(new == NULL)
+    {
+        printf("Can't inserted\n");
+    }
+    else
+    {
+        printf("Enter the data to insert in the last : ");
+        scanf("%d",&data);
+        new->data = data;
+        temp = *head;
+        if(temp == NULL){
+                new->data = data;
+                *head = NULL;
+                printf("Node has inserted in the last\n");
+                        }
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new;
+        new->next = NULL;
+        }
 }
 
-//find a link with given key
-struct node* find(int key) {
-
-   //start from the first link
-   struct node* current = head;
-
-   //if list is empty
-   if(head == NULL) {
-      return NULL;
-   }
-
-   //navigate through list
-   while(current->key != key) {
-	
-      //if it is last node
-      if(current->next == NULL) {
-         return NULL;
-      } else {
-         //go to next link
-         current = current->next;
-      }
-   }      
-	
-   //if data found, return the current Link
-   return current;
+void random_insert(struct node **head)
+{
+    struct node *new,*temp;
+    new = (struct node*)malloc(sizeof(struct node));
+    int data,i,loc;
+    if(new == NULL){
+        printf("Node not inserted\n");
+    }
+    else
+    {
+    printf("enter the data to insert randomly: ");
+    scanf("%d",&data);
+    new->data = data;
+    printf("Enter the position to insert the data : ");
+    scanf("%d",&loc);
+    temp = *head;
+    for(i=1; i<loc-1; i++)
+    {
+        temp = temp->next;
+        if(temp == NULL){
+            printf("Not able to insert the node because the entered position is exceeds the list\n");
+            return;
+        }
+    }
+    new->next = temp->next;
+    temp->next = new;
+    printf("Node has inserted in entered position\n");
+}
 }
 
-//delete a link with given key
-struct node* delete(int key) {
-
-   //start from the first link
-   struct node* current = head;
-   struct node* previous = NULL;
-	
-   //if list is empty
-   if(head == NULL) {
-      return NULL;
-   }
-
-   //navigate through list
-   while(current->key != key) {
-
-      //if it is last node
-      if(current->next == NULL) {
-         return NULL;
-      } else {
-         //store reference to current link
-         previous = current;
-         //move to next link
-         current = current->next;
-      }
-   }
-
-   //found a match, update the link
-   if(current == head) {
-      //change first to point to next link
-      head = head->next;
-   } else {
-      //bypass the current link
-      previous->next = current->next;
-   }    
-	
-   return current;
+int main()
+{
+    struct node *head = NULL;
+    int data,n,i,s;
+    printf("Enter the size of linked list : ");
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        printf("Enter %d value : ",i+1);
+        scanf("%d",&data);
+        add(&head, data);
+    }
+    printf("Forward linked list\n");
+    print(head);
+    reverse(&head);
+    printf("reverse linked list\n");
+    print(head);
+    last(&head);
+    printf("Last node insert\n"); 
+    print(head);
+    printf("Enter an element to insert in the begining : ");
+    scanf("%d",&s);
+    add(&head,s);
+    printf("node has inserted in the begining position\n");
+    print(head);
+    random_insert(&head);
+    printf("Random node insert for the existing list\n");
+    print(head);
+    printf("Addresses linking \n");
+    print_address(head);
+    
+    return 0;
 }
-
-void sort() {
-
-   int i, j, k, tempKey, tempData;
-   struct node *current;
-   struct node *next;
-	
-   int size = length();
-   k = size ;
-	
-   for ( i = 0 ; i < size - 1 ; i++, k-- ) {
-      current = head;
-      next = head->next;
-		
-      for ( j = 1 ; j < k ; j++ ) {   
-
-         if ( current->data > next->data ) {
-            tempData = current->data;
-            current->data = next->data;
-            next->data = tempData;
-
-            tempKey = current->key;
-            current->key = next->key;
-            next->key = tempKey;
-         }
-			
-         current = current->next;
-         next = next->next;
-      }
-   }   
-}
-
-void reverse(struct node** head_ref) {
-   struct node* prev   = NULL;
-   struct node* current = *head_ref;
-   struct node* next;
-	
-   while (current != NULL) {
-      next  = current->next;
-      current->next = prev;   
-      prev = current;
-      current = next;
-   }
-	
-   *head_ref = prev;
-}
-
-int main() {
-   insertFirst(1,10);
-   insertFirst(2,20);
-   insertFirst(3,30);
-   insertFirst(4,1);
-   insertFirst(5,40);
-   insertFirst(6,56); 
-
-   printf("Original List: "); 
-	
-   //print list
-   printList();
-
-   while(!isEmpty()) {            
-      struct node *temp = deleteFirst();
-      printf("\nDeleted value:");
-      printf("(%d,%d) ",temp->key,temp->data);
-   }  
-	
-   printf("\nList after deleting all items: ");
-   printList();
-   insertFirst(1,10);
-   insertFirst(2,20);
-   insertFirst(3,30);
-   insertFirst(4,1);
-   insertFirst(5,40);
-   insertFirst(6,56);
-   
-   printf("\nRestored List: ");
-   printList();
-   printf("\n");  
-
-   struct node *foundLink = find(4);
-	
-   if(foundLink != NULL) {
-      printf("Element found: ");
-      printf("(%d,%d) ",foundLink->key,foundLink->data);
-      printf("\n");  
-   } else {
-      printf("Element not found.");
-   }
-
-   delete(4);
-   printf("List after deleting an item: ");
-   printList();
-   printf("\n");
-   foundLink = find(4);
-	
-   if(foundLink != NULL) {
-      printf("Element found: ");
-      printf("(%d,%d) ",foundLink->key,foundLink->data);
-      printf("\n");
-   } else {
-      printf("Element not found.");
-   }
-	
-   printf("\n");
-   sort();
-	
-   printf("List after sorting the data: ");
-   printList();
-	
-   reverse(&head);
-   printf("\nList after reversing the data: ");
-   printList();
-
-   printf("Added for test");
-
-
-}
-
-
-/* hai veera
- * */
